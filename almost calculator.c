6d562baf18ac10b4c8766374cc8611e6a2a2d406
Rafat Abdullah Pantho <121303268+Rafat-Pantho@users.noreply.github.com>
2024-05-21 14:23:33 -0500

    Update almost calculator.c

--- almost calculator.c
+++ almost calculator.c
@@ -1,3 +1,5 @@
+// A code that reads the user input integers and operators as one single text
+
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
@@ -7,9 +9,14 @@ typedef long long ll;
 typedef unsigned int ui;
 
 #define f(i, n) for (ui i = 0; i < n; i++)
-#define MAX_SIZE 1000
+#define MAX_SIZE 100000
 
-ll operation(ll num1, ll num2, char op) {
+typedef struct store_result{
+    double result_of_calc;
+    int invalid;
+}store_result;
+
+double operation(double num1, double num2, char op) {
     switch (op) {
         case '+':
             return num1 + num2;
@@ -18,47 +25,49 @@ ll operation(ll num1, ll num2, char op) {
         case '*':
             return num1 * num2;
         case '/':
-            return num2 ? num1 / num2 : -1;
+            return num1 / num2;
         default:
             return 0;
     }
 }
 
-ll resulting(const char *user_input) {
-    ll numbers[MAX_SIZE] = {0};
+void resulting(store_result *res,const char *user_input) {
+    double numbers[MAX_SIZE] = {0};
     ui i = 0;
     char operators[MAX_SIZE] = {0};
 
     // Separating numbers and operators
     for (const char *ptr = user_input; *ptr != '\0'; ptr++) {
-        if (isdigit(*ptr)) {
-            numbers[i] = numbers[i] * 10 + (*ptr - '0');
-        } else {
+        if (isdigit(*ptr)) numbers[i] = numbers[i] * 10 + (*ptr - '0');
+        else {
             operators[i] = *ptr;
             i++;
         }
     }
 
     f(j, i) {
-        ll the_calculated = operation(numbers[0], numbers[j + 1], operators[j]);
-        if (the_calculated == -1) return -1;
-        numbers[0] = the_calculated;
+        if (numbers[j+1]==0 && operators[j]=='/'){
+            res->invalid=-1;
+            return;
+        }
+        numbers[0]=operation(numbers[0],numbers[j+1],operators[j]);
     }
-
-    return numbers[0];
+    res->result_of_calc=numbers[0];
 }
 
 int main() {
     char input_by_user[MAX_SIZE];
     scanf("%[^\n]", input_by_user);
-    
-    ll result = resulting(input_by_user);
-    if (result == -1) {
-        printf("Error: Division by zero\n");
-    }
-    else {
-        printf("%lld\n", result);
-    }
+
+    store_result res;
+
+    res.result_of_calc=0;
+    res.invalid=0;
+
+    resulting(&res, input_by_user);
+
+    if (res.invalid == -1) printf("Error: Division by zero\n");
+    else  printf("%g\n", res.result_of_calc);
 
     return 0;
 }
